5d88cb82352d53165fc8fd9156b0c421d932940d
Lais <lah_spfc2010@hotmail.com>
2016-05-29 12:32:26 -0300

    salvar os dados

--- js/Index.js
+++ js/Index.js
@@ -17,3 +17,36 @@ var Index = {
         }
     },
     
+    
+
+    //salva os dados
+    
+    saveUsuario: function(form){
+        var usuario = {};
+        usuario.nome = form.nome.value;
+        usuario.tcc = form.tcc.value;
+        usuario.orientador = form.orientador.value;
+        
+        if (UsuarioDAO.save(usuario/*, TableController.setTable()*/) == UsuarioDAO.NEW) {
+           TableController.addItem(usuario,Index.edit,Index.delete);
+        } else{ 
+           TableController.clearList();
+            Index.listUsuario();
+        }    
+               
+            form.nome.value = form.tcc.value = form.orientador.value = "";
+
+ },
+ 
+    setTable: function(){
+        var table = document.getElementById('tabela-usuarios');
+        TableController.setTable(table);
+    },
+        
+        listUsuario: function () {
+            Index.setTable();
+            var usuarioList = UsuarioDAO.retrieve();
+           if (usuarioList && usuarioList.length) {
+			TableController.addList(usuarioList, Index.edit, Index.delete);
+		}
+	},
